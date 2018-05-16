e31678cbec723fbad372ecfb3b28ec93743b37c2
melnyk.i.anton <melnyk.i.anton@outlook.com>
2018-05-16 17:18:18 +0300

    DLL+BLL

---
+++ Auction/BLL/Operations.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Operations
+    {
+        public void Registration(string name, string password, string email)
+        {
+            Users user = new Users();
+            user.Name = name;
+            user.PassWord = password;
+            user.Email = email;
+            user.Balance = 0;
+            using (Model1 m1 = new Model1())
+            {
+                if (m1.Userss.FirstOrDefault(x => x.Name == user.Name) == null)
+                {
+                    m1.Userss.Add(user);
+                }
+            }
+        }
+
+        public void AddBalance(Users user, double money)
+        {
+            user.Balance += money;
+        }
+
+        public void AddProduct(string name, int count, string type, int price)
+        {
+            Products pro = new Products();
+            pro.Name = name;
+            pro.Count = count;
+            pro.Type = type;
+            pro.Price = price;
+            using (Model1 m1 = new Model1())
+            {
+                m1.Productss.Add(pro);
+            }
+
+        }
+
+        public void BuyProduct(Users u, Products pro, int count)
+        {
+            using (Model1 m1 = new Model1())
+            {
+                if (pro.Count >= count && u.Balance >= pro.Price * count)
+                {
+                    u.Balance -= pro.Price * count;
+                    pro.Count = -count;
+
+                    Orders or = new Orders() { productsId = pro.ID, usersId = u.ID, DataOfOrder = DateTime.Now };
+                    m1.Orderss.Add(or);
+                    m1.SaveChanges();
+                }
+            }
+        }
+
+        public void DeleteProduct(string name, int count)
+        {
+            Products pro = new Products();
+            pro.Count = count;
+            using (Model1 m1 = new Model1())
+            {
+                m1.Productss.Remove(pro);
+            }
+        }
+
+        public List<Products> ShowAllProducts()
+        {
+            using (Model1 m1 = new Model1())
+            {
+                return m1.Productss.ToList();
+            }
+        }
+    }
+}
---
+++ Auction/DAL/Logger.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Logger
+    {
+        private static string LogPath = @"C:\Users\Admin\Desktop\NW\log.txt";
+        public static void Log(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(LogPath, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+            }
+        }
+    }
+
+    //namespace DALL
+    //{
+    //    using System;
+    //    using System.Collections.Generic;
+    //    using System.ComponentModel.DataAnnotations;
+    //    using System.Data.Entity;
+    //    using System.Linq;
+
+    //    public class Model1 : DbContext
+    //    {
+    //        public Model1()
+    //            : base("name=Model1")
+    //        {
+
+    //        }
+    //        public virtual DbSet<Users> Userss { get; set; }
+    //        public virtual DbSet<Products> Productss { get; set; }
+    //        public virtual DbSet<Orders> Orderss { get; set; }
+
+    //    }
+
+    //    public class Users
+    //    {
+    //        //public Users()
+    //        //{
+    //        //    Productss = new List<Products>();
+    //        //    Orderss = new List<Orders>();
+    //        //}
+    //        [Required]
+    //        public int ID { get; set; }
+    //        [Required]
+    //        public string Name { get; set; }
+    //        [Required]
+    //        public string PassWord { get; set; }
+
+    //        public double Balance { get; set; }
+    //        [Required]
+    //        public string Email { get; set; }
+
+    //        //public virtual ICollection<Products> Productss { get; set; }
+    //        //public virtual ICollection<Orders> Orderss { get; set; }
+    //    }
+
+    //    public class Products
+    //    {
+    //        //public Products()
+    //        //{
+    //        //    Userss = new List<Users>();
+    //        //}
+    //        [Required]
+    //        public int ID { get; set; }
+    //        [Required]
+    //        public string Name { get; set; }
+
+    //        public string Type { get; set; }
+    //        [Required]
+    //        public double Price { get; set; }
+    //        [Required]
+    //        public int Count { get; set; }
+
+    //        //public virtual ICollection<Users> Userss { get; set; }
+    //    }
+
+    //    public class Orders
+    //    {
+    //        //public Orders()
+    //        //{
+    //        //    Userss = new List<Users>();
+    //        //}
+    //        [Required]
+    //        public int ID { get; set; }
+    //        [Required]
+    //        public int usersId { get; set; }
+    //        [Required]
+    //        public int productsId { get; set; }
+    //        [Required]
+    //        public DateTime DataOfOrder { get; set; }
+    //        //public virtual ICollection<Users> Userss { get; set; }
+    //    }
+
+    //}
+}
