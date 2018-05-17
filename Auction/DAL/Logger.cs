6070086ff279db5cf61e2dc842ade1fc9e5fdea3
user <user@A13COMP4>
2018-05-17 17:39:47 +0300

    hz

---
+++ Auction/DAL/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Logger
+    {
+        private static string LogPath = @"C:\Users\Admin\Desktop\NW\log.txt";
+        public static void Log(string message)
+        {
+            using (StreamWriter sw = new StreamWriter(LogPath, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + ": " + message);
+            }
+        }
+    }
+}
