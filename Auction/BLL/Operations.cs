a8445b0e02dfcd0c55de1ba657fb327069a45c51
melnyk.i.anton <melnyk.i.anton@outlook.com>
2018-05-23 17:17:27 +0300

    123

--- Auction/BLL/Operations.cs
+++ Auction/BLL/Operations.cs
@@ -1,82 +1,107 @@
-﻿using DAL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
+using System.Reflection.Emit;
 
 namespace BLL
 {
     public class Operations
     {
-        public void Registration(string name, string password, string email)
+        public static bool Registration(string name, float balance, string password, string email, int age)
         {
-            User user = new Users
-            {
-                Name = name,
-                PassWord = password,
-                Email = email,
-                Balance = 0
-            };
+            User user = new User();
+            user.NickName = name;
+            user.Password = password;
+            user.Email = email;
+            user.Age = age;
+            user.Balance = balance;
+            //asd
             using (Model1 m1 = new Model1())
             {
-                if (m1.Userss.FirstOrDefault(x => x.Name == user.Name) == null)
+                if (m1.Users.FirstOrDefault(x => x.NickName == user.NickName) == null)
                 {
-                    m1.Userss.Add(user);
+                    m1.Users.Add(user);
+                    m1.SaveChanges();
+                    return true;
                 }
+                else return false;
             }
         }
 
-        public void AddBalance(Users user, double money)
+        public static bool SignIn(string nickname, string pass, Label l)
+        {
+            using (Model1 m1 = new Model1())
+            {
+                if (m1.Users.FirstOrDefault(x => x.NickName == nickname && x.Password == pass) != null)
+                    return true;
+                else
+                    //Zapusatu v Label "Error or False password, NickName" ;
+                    return false;
+            }
+            //Roma dayn
+            //User u = m1.Users.FirstOrDefault(x => x.NickName == nickname && x.Password == pass);
+            //return u;
+        }
+
+        public static void AddBalance(User user, float money)
         {
             user.Balance += money;
         }
 
-        public void AddProduct(string name, int count, string type, int price)
+        public static void AddItem(string name, int price, string info, User user)
         {
-            Products pro = new Products();
-            pro.Name = name;
-            pro.Count = count;
-            pro.Type = type;
-            pro.Price = price;
+            Item item = new Item();
+            item.Price = price;
+            item.Name = name;
+            item.Info = info;
+            item.User = user;
             using (Model1 m1 = new Model1())
             {
-                m1.Productss.Add(pro);
-            }
-
-        }
-
-        public void BuyProduct(Users u, Products pro, int count)
-        {
-            using (Model1 m1 = new Model1())
-            {
-                if (pro.Count >= count && u.Balance >= pro.Price * count)
-                {
-                    u.Balance -= pro.Price * count;
-                    pro.Count = -count;
-
-                    Orders or = new Orders() { productsId = pro.ID, usersId = u.ID, DataOfOrder = DateTime.Now };
-                    m1.Orderss.Add(or);
-                    m1.SaveChanges();
-                }
+                m1.Items.Add(item);
+                m1.SaveChanges();
             }
         }
 
-        public void DeleteProduct(string name, int count)
+        public void AddAuction(Item item, string name)
         {
-            Products pro = new Products();
-            pro.Count = count;
+            Auction a = new Auction();
+            a.Name = name;
+            a.DateOfStart = DateTime.Now;
+            a.Item = item;
+
             using (Model1 m1 = new Model1())
             {
-                m1.Productss.Remove(pro);
+                m1.Auctions.Add(a);
+                m1.SaveChanges();
             }
         }
 
-        public List<Products> ShowAllProducts()
+        public void DelAuction(Auction a)
         {
             using (Model1 m1 = new Model1())
             {
-                return m1.Productss.ToList();
+                m1.Auctions.Remove(a);
+                m1.SaveChanges();
+            }
+        }
+
+        public void DeleteItem(Item item)
+        {
+            using (Model1 m1 = new Model1())
+            {
+                m1.Items.Remove(item);
+                m1.SaveChanges();
+            }
+        }
+
+        public List<Auction> ShowAllAuctions()
+        {
+            using (Model1 m1 = new Model1())
+            {
+                return m1.Auctions.ToList();
             }
         }
     }
